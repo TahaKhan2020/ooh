dfc5f200133899e48316dd50b366aa76670261f5
Taha Khan <128257617+ssuet187@users.noreply.github.com>
2023-06-20 21:42:21 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import AboutMe from './component/about-me'
-import {aboutMe,skills,professionalData} from '../data/page-data'
+import {aboutMe,skills,professionalData, AcademicData,Certification,projects,activities,languages} from '../data/page-data'
 import ProfessionaExperience from './component/professiona-experience'
+import Academic from './component/academic'
+import Certifications from './component/Certifications'
+import Project from './component/Projects'
+import CurriAct from './component/curriact'
+import Languages from './component/language'
 const page = () => {
   return (
     <div>
       
       <AboutMe data={aboutMe} skills={skills}/>
       <ProfessionaExperience data={professionalData}/>
+      <Academic data={AcademicData}/>
+      <Certifications data={Certification}/>
+      <Project data={projects}/>
+      <CurriAct data={activities}/>
+      <Languages data={languages}/>
     </div>
   )
 }
